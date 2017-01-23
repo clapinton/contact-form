5d88190be53c76f268f4e2150e4ab9de68fa895d
Clapinton <eric.olivetree@gmail.com>
2017-01-22 20:01:23 -0800

    Exclude the index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do 
-    resources :messages, only: [:index, :create, :show]
+    resources :messages, only: [:create]
   end
 
 end
